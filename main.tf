f5a07869b3c4bf6422a8e3303dc929abe542ebbd
blackkolly <komolafe.solomon@yahoo.com>
2023-01-08 12:57:44 +0100

    vpc added

--- main.tf
+++ main.tf
@@ -1,53 +1,163 @@
+/*1 .Create custom VPC
+2. Create custom Subnet
+3. Create Route Table & internet Gateway
+4. Provision Ec2 Instance
+5. Deploy nginx Docker container
+6. Create Security Group (Firewall)*/
+
+
+
 provider "aws" {
   region = "us-east-1"
-  //access_key = "AKIA5TKXC5FLNJOYPNU3"
-  //secret_key = "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"
-  
 }
-variable "cidr_block" {
-  description = "cidr block for subnet1 and subnet2"
-  type = list(string)
-  
-}
-//Creating varibles
-//variable "subnet_cidr_block_2" {
-  //description = "subnet cidr block 2"
-  
+variable "vpc_cidr_block" {}
+variable "avail_zone" {}
+variable "subnet_cidr_block" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
 
-
-resource "aws_vpc" "development-vpc" {
-  cidr_block = "10.0.0.0/16"
-  
-}
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
-  cidr_block = var.cidr_block[0]
-  availability_zone = "us-east-1a"
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-     Name: "developments",
-     vpc_env: "dev"
+    Name: "${var.env_prefix}-vpc"
+    
+  }
+  
+  
+}
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+    
   }
   
 }
-//Creating subnet on existing vcp,use Data Resources-It helps us to query the existing
-data "aws_vpc" "existing_vpc" {
-  default = true
-}
+resource "aws_internet_gateway" "myapp-igwa" {
+  vpc_id = aws_vpc.myapp-vpc.id
 
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block = var.cidr_block[1]
-  availability_zone = "us-east-1a"
   tags = {
-     Name: "subnet-1-default"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
-  
+/*resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igwa.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
 }
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
-  
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}*/
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igwa.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
 }
+
+/*resource "aws_security_group" "myapp_sg" {
+  name = "myapp_sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}*/
+// Creating default security group
+  resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+
+  }
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["137112412989"] # Canonical
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default_sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = "webserver"
+  
+
+tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+  
